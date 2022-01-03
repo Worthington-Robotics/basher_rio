45763def0ec9d64088d5a73de4055ec475b47f4e
PhillipWhiter <boring.someone@gmail.com>
2022-01-02 19:43:03 -0500

    added javadoc for class and constructor in the SModule class

--- src/main/include/subsystems/SModule.h
+++ src/main/include/subsystems/SModule.h
@@ -11,6 +11,13 @@
 namespace robot
 {
 
+    /**
+     * A struct for encoding the PIDF gains of a control loop
+     * @param p the proportional response to error (ex. kp * error / 1023 = motor responce)
+     * @param i the integrated response of the error (ex. ki * S error dt / 1023 = motor responce) [This is VERY DANGEROUS and should be used with an iAccum]
+     * @param d the derivative of error (ex. kd * [d/dt] error / 1023 = motor responce)
+     * @param f the feedforward response to the setpoint (ex. kf * setpoint / 1023 = motor responce)
+     */
     struct PIDF
     {
         double p;
@@ -19,6 +26,13 @@ namespace robot
         double f;
     };
 
+    /**
+     * A struct for encoding the sensor data of a give swerve module, including information for the angle, drive, and CANcoder sections
+     * @param angleRel the relative ticks of the given motor, should boot to zero if inline after being adjusted by startup offsets
+     * @param drivePos the position of the drive motor in ticks, NOT the position of the robot
+     * @param driveVel the velocity of the drive motor in ticks / 100ms NOT the speed of the robot
+     * @param encAbs the absolute position of the CANcoder, and the refrence point for angleRel
+     */
     struct sSensorData
     {
         double angleRel;
@@ -27,11 +41,25 @@ namespace robot
         double encAbs;
     };
 
+    /**
+     * SModule is a class designed to house all information assosiated with a single swerve module on the robot
+     * It contains two CTRE TalonFX motors, and one CTRE CANcoder working in tandem to excecute swerveStates
+     * as defined by WPI's swerveState class 
+     */
     class SModule
     {
         public:
 
-        SModule(int, int, int, double, PIDF, PIDF);
+        /** The constructor for a SModule object
+         * @param driveID the ID for a single CTRE TalonFX motor controller, which controls the linear velocity of the module
+         * @param angleID the ID for a single CTRE TalonFX motor controller, which controls the angular position of the module
+         * @param encoderID
+         * @param offset
+         * @param dValues
+         * @param aValues
+         */
+        SModule(int driveID, int angleID, int encoderID, double offset, PIDF dValues, PIDF aValues);
+
         /**
          * Override this function with all the nessecary code needed to reset a subsystem
          **/
